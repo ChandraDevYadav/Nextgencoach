7cdd3a0bceb61e3ff8212a5971bef85bd68af624
Chandra Dev Yadav <yadavchandradev2000@gmail.com>
2025-05-27 14:00:33 +0545

    Navbar Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,11 +12,13 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { Toaster } from "react-hot-toast";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Toaster position="top-right" />
+      <Navbar />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
